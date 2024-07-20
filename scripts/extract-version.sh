cb0798404455f703884149fb83a2938e58ff3d0f
Aditya Sumardi <adityasumardi@sumtechconsulting.co.id>
2024-07-20 11:31:09 +0700

    add: library expose version

--- scripts/extract-version.sh
+++ scripts/extract-version.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Ensure the path is correct
+BUILD_GRADLE_PATH="app/build.gradle.kts"
+
+if [ ! -f "$BUILD_GRADLE_PATH" ]; then
+  echo "Error: $BUILD_GRADLE_PATH not found!"
+  exit 1
+fi
+
 # Extract versionCode
-        versionCode=$(grep "versionCode" app/build.gradle.kts | awk '{print $2}')
+versionCode=$(grep "versionCode" $BUILD_GRADLE_PATH | awk '{print $2}')
 
 # Extract versionName
-        versionName=$(grep "versionName" app/build.gradle.kts | awk '{print $2}' | tr -d '"')
+versionName=$(grep "versionName" $BUILD_GRADLE_PATH | awk '{print $2}' | tr -d '"')
 
 # Print the extracted values
-        echo "VERSION_CODE=$versionCode"
+echo "VERSION_CODE=$versionCode"
 echo "VERSION_NAME=$versionName"
